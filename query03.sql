9ed7b7be4d3fac3fcbd4c3853a0cdf482fb005b4
Haobing Liu <ishbliu@hotmail.com>
2023-03-30 12:35:58 -0400

    Update query03.sql

--- query03.sql
+++ query03.sql
@@ -1,3 +1,11 @@
+alter table phl.pwd_parcels
+alter column geog type geography
+using ST_Transform(geog::geometry, 4269);
+
+alter table septa.bus_stops
+alter column geog type geography
+using ST_Transform(geog::geometry, 4269);
+
 WITH
 
 parcels AS (
@@ -5,6 +13,7 @@ parcels AS (
       geog,
       address
     FROM phl.pwd_parcels
+    WHERE ST_Intersects(geog, ST_MakeEnvelope(-75.3, 39.9, -74.9, 40.2, 4269))
   ),
 
 bus_stops AS (
@@ -16,13 +25,13 @@ bus_stops AS (
   ),
   
 closest_bus_stop AS (
-    SELECT DISTINCT ON (p.address)
-      p.address,
-      bs.stop_name,
-      ST_Distance(p.geog, bs.geog) AS distance
-    FROM parcels p
-    JOIN bus_stops bs ON ST_Distance(p.geog, bs.geog) < 800 
-    ORDER BY p.address, ST_Distance(p.geog, bs.geog)
+    SELECT DISTINCT ON (parcels.address)
+      parcels.address,
+      bus_stops.stop_name,
+      ST_Distance(parcels.geog, bus_stops.geog) AS distance
+    FROM parcels 
+    JOIN bus_stops ON ST_DWithin(parcels.geog, bus_stops.geog, 800) 
+    ORDER BY parcels.address, ST_Distance(parcels.geog, bus_stops.geog)
   )
   
 SELECT
@@ -31,3 +40,4 @@ SELECT
   cb.distance
 FROM closest_bus_stop cb
 ORDER BY cb.distance DESC;
+
